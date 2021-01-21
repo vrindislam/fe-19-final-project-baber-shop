f2858381fd17bc59ec8a3da78b2a2fd818c1553e
Anton Sypavka <anton.sypavka@gmail.com>
2021-01-21 15:34:55 +0200

    Product card

--- client/src/components/ProductCard/index.js
+++ client/src/components/ProductCard/index.js
@@ -1,40 +1,40 @@
 import React from 'react';
+import { useDispatch } from "react-redux";
 import './styles.less';
 import { Card, Button } from 'antd';
+import {cartAction} from "../../store/cart/cartAction";
 
 const ProductCard = ({ product }) => {
-    console.log('Product from ProductCard -->', product);
-    const {name, imageUrls, currentPrice, itemNo, categories} = product;
 
-    const onAddToCart = (event) => {
-        event.preventDefault();
-        const { target } = event;
-        const itemNo = target.closest('.ant-card').dataset.itemno;
-        console.log('Target-->', target);
-        console.log('ItemNo-->', itemNo);
-        console.log('Product-->', product);
+    const {name, imageUrls, currentPrice, itemNo, categories, productUrl} = product;
+    const dispatch = useDispatch();
 
+    const onAddToCart = (e) => {
+        e.preventDefault();
+        dispatch(cartAction(product));
     }
 
     return (
         <>
             <li className='product-card-container'>
-                <Card data-category={categories} data-itemno={itemNo} bordered={true} style={{ width: 305, height: 272, position: 'relative' }} hoverable={true} cover={
-                    <img
-                        className='product-card-img'
-                        alt="example"
-                        src={imageUrls[0]}
-                    />
-                }
-                >
-                    <div className='product-heading-wrapper'>
-                        <p className='product-heading'>{name}</p>
-                        <p className='product-price'>{currentPrice}$</p>
-                    </div>
-                    <div className='button-container'>
-                        <Button type="primary" className='product-card-btn' onClick={onAddToCart}>Add to cart</Button>
-                    </div>
-                </Card>
+                <a href={productUrl}>
+                    <Card data-category={categories} src='google.com' data-itemno={itemNo} bordered={true} style={{ width: 305, height: 272, position: 'relative' }} hoverable={true} cover={
+                        <img
+                            className='product-card-img'
+                            alt="product-item"
+                            src={imageUrls[0]}
+                        />
+                    }
+                    >
+                        <div className='product-heading-wrapper'>
+                            <p className='product-heading'>{name}</p>
+                            <p className='product-price'>{currentPrice}$</p>
+                        </div>
+                        <div className='button-container'>
+                            <Button type="primary" className='product-card-btn' onClick={onAddToCart}>Add to cart</Button>
+                        </div>
+                    </Card>
+                </a>
             </li>
         </>
     )
--- client/src/store/rootReducer.js
+++ client/src/store/rootReducer.js
@@ -1,10 +1,13 @@
 import { combineReducers } from 'redux'
 import testReducer from './test_store/testReducer'
 import preloaderReducer from './preloader/preloaderReducer'
+import cartReducer from "./cart/cartReducer";
+
 
 const rootReducer = combineReducers({
   testProduct: testReducer,
   preloader: preloaderReducer,
+  cart: cartReducer,
 })
 
 export default rootReducer
