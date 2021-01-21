60272da51d8b58e226ec2da46b98cc7147e7b765
KosSid <54644828+KosSid@users.noreply.github.com>
2021-01-21 14:22:40 +0200

    admin_form_created

--- client/src/components/Forms/CategoryForm/index.js
+++ client/src/components/Forms/CategoryForm/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import './style.less';
 import { Form, Input, Button} from 'antd';
 
-
+// form layout settings
 const layout = {
   labelCol: {
     span: 24,
@@ -11,15 +11,15 @@ const layout = {
     span: 24,
   },
 };
-
 const tailLayout = {
   wrapperCol: {
     span: 24,
   },
 };
 
-const rules = [{required: true, message: 'Please input your username'}];
+// constants to create form input fields
 
+const rules = [{required: true, message: 'Please input your username'}];
 const fieldsSetArr = [
   {label: "Category ID", name: "id", rules, hidden: false},
   {label: "Category Name", name: "name", rules, hidden: false},
@@ -29,45 +29,27 @@ const fieldsSetArr = [
   {label:"Parent ID", name:"parentId", hidden: true}
 ]
 
+
 const CategoryForm =() => {
-  // const [fieldValues, setFieldValues] = useState({
-  //   id: '',
-  //   name: '',
-  //   description: '',
-  //   imgUrl: 'TBD_img/catalog/women.png',
-  //   level: 0,
-  //   parentId: 'null',
-  // })
   const [form] = Form.useForm()
 
-  // useEffect(() => {
-  //   console.log(fieldValues);
-  // }, [fieldValues])
-
+  // function to create form input fields based on constants
   const setUpFormFields = () => fieldsSetArr.map(category => (
     <Form.Item key={category.name} {...category}>
       <Input placeholder={`input ${category.label}`} />
     </Form.Item>
   ))
 
+  // handle form on succesful submit
   const onFinish = (values) => {
     console.log('Values =>>>>>', values);
-    // console.log('fieldValues =>>>>>', fieldValues);
   };
 
+  // handle form on faild submit
   const onFinishFailed = (errorInfo) => {
     console.log('Failed:', errorInfo);
   };
 
-  // const handleChanges = (value) => {
-  //   const [name, val] = Object.entries(value)[0];
-  //   setFieldValues(prev => {
-  //     return {
-  //       ...prev,
-  //       [name]: val
-  //     }
-  //   })
-  // }
 
   return (
     <Form
@@ -85,7 +67,6 @@ const CategoryForm =() => {
       }}
       onFinish={onFinish}
       onFinishFailed={onFinishFailed}
-      // onValuesChange={handleChanges}
     >
       {setUpFormFields()}
       <Form.Item {...tailLayout}>
