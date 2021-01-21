f97d441b675e3c2706c7449b3d3a3299ee4e3b77
KosSid <54644828+KosSid@users.noreply.github.com>
2021-01-21 02:04:41 +0200

    form for category update

--- client/src/pages/Admin/AdminCategory/index.js
+++ client/src/pages/Admin/AdminCategory/index.js
@@ -1,15 +1,13 @@
 import React, {useEffect} from "react";
-import { Layout, Menu } from "antd";
-import {Link} from 'react-router-dom';
-import { ContainerOutlined, DatabaseOutlined, AccountBookOutlined } from "@ant-design/icons";
+import { Layout, Row, Col, Divider } from "antd";
 import "./styles.less";
+import AdminSider from "../../../components/AdminSider";
+import CategoryForm from "../../../components/Forms/CategoryForm";
 
 
-const { SubMenu, Item } = Menu;
-const { Content, Sider } = Layout;
+const { Content} = Layout;
 
 const AdminCatergory = (props) => {
-  const defaultSelected = 'category';
 
   useEffect(() => {
     console.log('AdminCategory did Mount');
@@ -18,46 +16,16 @@ const AdminCatergory = (props) => {
 
   return (
     <Layout className="admin-category-container">
-      <Sider width={210} className="sideBar-container">
-        <Menu
-          mode="inline"
-          defaultSelectedKeys={[defaultSelected]}
-          defaultOpenKeys={["sub1", "sub2", "sub3"]}
-          style={{ height: "100%", borderRight: 0 }}
-        >
-          <SubMenu key="sub1" icon={<ContainerOutlined />} title="Manage Categories">
-            <Item key="category">
-              <Link to='/admin/category'>Category</Link>
-            </Item>
-            <Item key="sub">
-              <Link to='/admin/sub'>Sub-Category</Link>
-            </Item>
-          </SubMenu>
-          <SubMenu key="sub2" icon={<DatabaseOutlined />} title="Manage Product">
-            <Item key="product">
-              <Link to='/admin/product'>Product</Link>
-            </Item>
-            <Item key="products">
-              <Link to='/admin/products'>Products</Link>
-            </Item>
-          </SubMenu>
-          <SubMenu key="sub3" icon={<AccountBookOutlined/>} title="Other">
-            <Item key="dashbord">
-              <Link to='/admin/dashbord'>Dashbord</Link>
-            </Item>
-            <Item key="password">
-              <Link to='/admin/password'>Manage Password</Link>
-            </Item>
-          </SubMenu>
-        </Menu>
-      </Sider>
-      <Layout>
-        <Content
-          className="category-content-container"
-        >
-          Content
+      <AdminSider/>
+        <Content className="category-content-container">
+          <Divider orientation="left">Create Category</Divider>
+          <Row gutter={16}>
+            <Col span={24}>
+              <CategoryForm/>
+            </Col>
+          </Row>
         </Content>
-      </Layout>
+
     </Layout>
   );
 };
