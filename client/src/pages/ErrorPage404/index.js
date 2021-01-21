9501f7763d534c92a11fa8b0ed90f08f48a396de
Alex Patsalov <alexpatsalov@gmail.com>
2021-01-21 12:08:45 +0200

    404 page and redirect ready

--- client/src/pages/ErrorPage404/index.js
+++ client/src/pages/ErrorPage404/index.js
@@ -1,25 +1,27 @@
 import React from 'react';
 import './styles.less';
-import { Result, Button } from 'antd';
-import {Redirect} from "react-router-dom";
+import {Result} from 'antd';
+import {withRouter} from 'react-router-dom'
 
-const redirect = () => {
-    console.log('click');
-    return(
-        <Redirect push to='/home'/>
-    )
-}
+const Btn = withRouter(({history}) => {
+
+    return (
+        <button type='primary' onClick={() => {
+            history.push('/home')
+        }}> Back to Home page!</button>
+    );
+})
 
 const ErrorPage = (props) => {
     return (
-        <Result
-            status="404"
-            title="404"
-            subTitle="Something went wrong, boy"
-            extra={<Button onClick={redirect} type="primary">Back Home</Button>}
-        />
-
-
+        <>
+            <Result
+                status="404"
+                title="404"
+                subTitle="Something went wrong, boy"
+                extra={<Btn/>}
+            />
+        </>
     );
 }
 export default ErrorPage;
