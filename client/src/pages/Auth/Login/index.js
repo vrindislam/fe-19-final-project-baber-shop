86b9199e67844fa6ae7dc8ee7bdb772085bfd9a4
Sofiia Franchuk <sof.franko03@gmail.com>
2021-01-21 18:58:11 +0200

    test commit again

--- client/src/pages/Auth/Login/index.js
+++ client/src/pages/Auth/Login/index.js
@@ -3,7 +3,7 @@ import './styles.less'
 
 function Login () {
   return (
-      <div>Login Page <span onClick={test}>test push</span></div>
+      <div>Login Page <span onClick={test}>test push</span> <h1>Commit now</h1></div>
 
   )
 }
