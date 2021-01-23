7cb92a8fbcc8e67c9c7a1d061c073038dcf68981
Alex Patsalov <alexpatsalov@gmail.com>
2021-01-23 12:31:27 +0200

    Link 'home' repaired

--- client/src/routes/MainRoutes.js
+++ client/src/routes/MainRoutes.js
@@ -11,7 +11,7 @@ import ErrorPage from "../pages/ErrorPage404";
 const MainRoutes = () => {
     return (
         <Switch>
-            <Route exact path={'/home'} component={Home}/>
+            <Route exact path={'/'} component={Home}/>
             <Route exact path={'/register'} component={Register}/>
             <Route exact path={'/login'} component={Login}/>
             <Route exact path={'/cart'} component={Cart}/>
