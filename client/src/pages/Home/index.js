3fe59f361e9e6c58f5e463013ede391d6d936c25
Khrystyna Tverdokhlib <xrustja90@gmail.com>
2021-01-24 22:53:40 +0200

    Footer 1st try

--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -1,16 +1,16 @@
-
 import React from 'react'
 import './styles.less'
 import Slider from "../../components/Slider";
 import ProductList from "../../components/ProductList";
-
+import Footer from "../../components/Footer";
 const Home = (props) => {
     return (
         <div>
-        <h2>This is Home Page</h2>
-        <Slider/>
-    <ProductList/>
-  </div>
+            <h2>This is Home Page</h2>
+            <Slider/>
+            <ProductList/>
+            <Footer/>
+        </div>
     )
 }
 
