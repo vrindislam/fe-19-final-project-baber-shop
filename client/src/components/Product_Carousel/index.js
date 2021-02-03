48f3bf9bdad86746177be2fbc395ec8f45801e10
Anton Sypavka <anton.sypavka@gmail.com>
2021-02-03 15:33:32 +0200

    Product Carousel: without styles

--- client/src/components/Product_Carousel/index.js
+++ client/src/components/Product_Carousel/index.js
@@ -6,19 +6,19 @@ import './styles.less';
 const images = [
     {
         original: 'https://picsum.photos/id/1018/1000/600/',
-        thumbnail: 'https://picsum.photos/id/1018/250/150/',
+        thumbnail: 'https://picsum.photos/id/1018/1000/600/',
+    },
+    {
+        original: 'https://picsum.photos/id/1019/1000/600/',
+        thumbnail: 'https://picsum.photos/id/1019/1000/600/',
     },
     {
         original: 'https://picsum.photos/id/1015/1000/600/',
-        thumbnail: 'https://picsum.photos/id/1015/250/150/',
+        thumbnail: 'https://picsum.photos/id/1015/1000/600/',
     },
     {
         original: 'https://picsum.photos/id/1019/1000/600/',
-        thumbnail: 'https://picsum.photos/id/1019/250/150/',
-    },
-    {
-        original: 'https://picsum.photos/id/1019/1000/600/',
-        thumbnail: 'https://picsum.photos/id/1019/250/150/',
+        thumbnail: 'https://picsum.photos/id/1019/1000/600/',
     },
 ];
 
