98e0023df71d643a0861b3dcb8406e8bf8a0a2ca
KosSid <54644828+KosSid@users.noreply.github.com>
2021-02-04 14:53:33 +0200

    cleaning in the CategoryUpdateForm

---
+++ client/src/components/Forms/CategoryUpdateForm/constants.js
@@ -0,0 +1,24 @@
+// Create-Form Schema and controls rules
+const rules = [{required: true, message: 'field is required'}];
+
+export const fieldsSetArr = [
+  ['input', {label: "Category Name", name: "name", rules}],
+  ['input', {label: "Category Description", name: "description", rules}],
+  ['input', {label: "Image URL", name: "imgUrl", rules}]
+]
+
+// form layout settings
+export const layout = {
+  labelCol: {
+    span: 24,
+  },
+  wrapperCol: {
+    span: 24,
+  },
+};
+export const tailLayout = {
+  wrapperCol: {
+    span: 24,
+  },
+};
+
