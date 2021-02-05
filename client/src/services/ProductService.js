a6a9e143d02c40a065b03637da3ac94a47a830a4
Sergey Tverdokhleb <selik.oo@gmail.com>
2021-02-05 23:35:13 +0200

    ProductService fix

--- client/src/services/ProductService.js
+++ client/src/services/ProductService.js
@@ -2,7 +2,6 @@ import Ajax from "./Ajax";
 
 class ProductService {
     async getProductsForBanner(category = {id: 'razor'}) {
-        console.log(category.id);
         const response = await Ajax.get(`/products/filter?category=${category.id}`);
         if (response.productsQuantity > 0) {
             return response.products.splice(0,4);
