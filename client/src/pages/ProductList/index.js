b19dfb9ee7f5aaeccf75eea0c56a1f2582282826
Alex Patsalov <alexpatsalov@gmail.com>
2021-02-09 19:06:38 +0200

    dynamic url on its way

--- client/src/pages/ProductList/index.js
+++ client/src/pages/ProductList/index.js
@@ -1,4 +1,5 @@
 import React, {useEffect, useState} from 'react';
+import {useHistory, useLocation, useParams} from "react-router";
 import './styles.less';
 import CheckboxFilter from "../../components/CheckboxFilters";
 import PriceSlider from "../../components/PriceSlider";
@@ -8,15 +9,26 @@ import FilteredProducts from "../../components/FilteredProducts";
 
 const ProductList = () => {
 
+    const queryString = require('query-string');
+    const {queryStringUrl} = useParams();
+    const history = useHistory();
+    const location = useLocation();
+
     const [checkboxFiltersDB, setCheckboxFiltersDB] = useState([]);
     const [checkboxFiltersClicked, setCheckboxFiltersClicked] = useState([]);
     const [minValue, setMinValue] = useState(100);
     const [maxValue, setMaxValue] = useState(700);
     const [showFilters, setShowFilters] = useState(false);
-    const queryString = require('query-string');
+    const [parsedUrl, setParsedUrl] = useState({});
     const values = pickUpValues(checkboxFiltersClicked);
     const groupedValues = groupValues(values);
-    const string = queryString.stringify({...groupedValues, ...{minPrice: minValue, maxPrice: maxValue}}, {arrayFormat: "comma"});
+    const string = queryString.stringify({
+        ...groupedValues, ...{
+            minPrice: minValue,
+            maxPrice: maxValue
+        }
+    }, {arrayFormat: "comma"});
+    console.log('querystring--->>', string);
 
     useEffect(() => {
         async function fetch() {
@@ -25,6 +37,22 @@ const ProductList = () => {
         }
         fetch()
     }, []);
+
+    useEffect(() => {
+        const parsedString = queryString.parse(queryStringUrl, {arrayFormat: 'comma'});
+        setParsedUrl(parsedString);
+        console.log('path--->>', location.pathname);
+        console.log('parsedString-->', parsedString);
+        console.log('parsedString from state', parsedUrl);
+        // location.pathname += string;
+        history.push(location.pathname);
+        console.log('location-->', location);
+    }, [history, location, parsedUrl, queryString, queryStringUrl, string]);
+
+    useEffect(() => {
+        setMinValue(parsedUrl.minPrice);
+        setMaxValue(parsedUrl.maxPrice);
+    }, [parsedUrl.maxPrice, parsedUrl.minPrice])
 
     const catchCheckbox = (e) => {
         if (e.target.type === 'checkbox') {
@@ -40,28 +68,27 @@ const ProductList = () => {
             }
         }
     }
-
-    const openFilters = () =>{
+    const openFilters = () => {
         setShowFilters(!showFilters);
     }
-
     const show = showFilters ? 'active' : 'hidden';
-    const showButton = {display: showFilters? 'none' : 'inline-block'}
-
-
+    const showButton = {display: showFilters ? 'none' : 'inline-block'}
 
     return (
         <>
             <div className="product-list-container">
-                <div className={"filters-container " + show} >
-                    <PriceSlider minValue={minValue} maxValue={maxValue} setMinVal={setMinValue} setMaxVal={setMaxValue}/>
+                <div className={"filters-container " + show}>
+                    <PriceSlider minValue={minValue || parsedUrl.minPrice} maxValue={maxValue || parsedUrl.maxPrice}
+                                 setMinVal={setMinValue}
+                                 setMaxVal={setMaxValue}/>
                     <CheckboxFilter filters={checkboxFiltersDB} clickCheckbox={catchCheckbox}/>
                 </div>
                 <div className="open-filters-btn-container">
-                    <button type='button' className='open-filters-btn' style={showButton} onClick={openFilters}>X</button>
+                    <button type='button' className='open-filters-btn' style={showButton} onClick={openFilters}>X
+                    </button>
                     <button type='button' className={'open-filters-btn ' + show} onClick={openFilters}>O</button>
                 </div>
-                    <FilteredProducts queryString={string}/>
+                <FilteredProducts queryString={string}/>
             </div>
         </>
     )
