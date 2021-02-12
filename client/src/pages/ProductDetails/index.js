67d24f9a18a97b7805829c626d53c233eecbd122
Anton Sypavka <anton.sypavka@gmail.com>
2021-02-12 20:47:27 +0200

    Product Carousel: Merged with last updates in Develop branch

--- client/src/pages/ProductDetails/index.js
+++ client/src/pages/ProductDetails/index.js
@@ -1,16 +1,18 @@
 import React, {useEffect, useState} from 'react';
-import './styles.less';
 import ProductCarousel from "../../components/ProductCarousel";
 import Ajax from "../../services/Ajax";
+
+import './styles.less';
 
 const ProductDetails = (props) => {
     const [images, setImages] = useState([])
 
-    useEffect( () => {
-        async function fetch () {
-            const { imageUrls } = await Ajax.get('/products/76100')
+    useEffect(() => {
+        async function fetch() {
+            const {imageUrls} = await Ajax.get('/products/76100')
             setImages(imageUrls);
         }
+
         fetch();
     }, [])
 
