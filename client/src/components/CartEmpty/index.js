4d0fd8bd47ac58fe349036441a3481379b125d12
Serhii Peksymov <peksymov@gmail.com>
2021-02-26 12:55:30 +0200

    minor fixes

--- client/src/components/CartEmpty/index.js
+++ client/src/components/CartEmpty/index.js
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 import "./styles.less"
 const EmptyCartPage = () => {
   return (
-      <Result
+      <Result className="empty-cart_wrapper"
         icon={<ShoppingCartOutlined className="empty-cart_icon"/>}
         title="Cart is empty"
         extra={<Link to={"/shop"}><Button className="empty-cart_button" type="primary">Go to shop</Button></Link>
